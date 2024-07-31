387cf12fbdd55fafbc8506e940675e19dfbd78b4
Andrew Wu <arw@dmp.com.tw>
2024-07-31 20:38:28 +0800

    clone-repo.sh: Set coreboot to correct branch.

--- clone-repo.sh
+++ clone-repo.sh
@@ -6,6 +6,7 @@ BASE_DIR=`dirname $0`
 git clone ${COREBOOT_REPO} coreboot && \
 git clone ${SEABIOS_REPO} seabios && \
 cd coreboot && \
+git checkout 86duino-bios-0.9beta-oss && \
 wget -O .git/hooks/commit-msg http://review.coreboot.org/tools/hooks/commit-msg && \
 chmod +x .git/hooks/commit-msg
 
